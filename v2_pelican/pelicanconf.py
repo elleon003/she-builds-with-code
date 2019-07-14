f9daa0aefc7d52d484765c871790d62466e8ff90
elleon003 <ngrandison@ymail.com>
2019-07-14 17:06:59 -0400

    Finished Pelican Theme Index & base.html

--- v2_pelican/pelicanconf.py
+++ v2_pelican/pelicanconf.py
@@ -31,7 +31,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 9
+DEFAULT_PAGINATION = 6
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
